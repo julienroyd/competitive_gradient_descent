ac481f093b9f51d28a98659cd7187771f7ddab73
JulienRoy <jul.roy1311@gmail.com>
2019-12-13 17:58:53 -0500

    Experiment1: Refactored all 9 runs in a single graph

--- notebooks/experiment1.py
+++ notebooks/experiment1.py
@@ -1,6 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+import itertools
 
 
 # The problem
@@ -14,14 +15,17 @@ def g(x, y, alpha):
 
 x_init = 0.5
 y_init = 0.5
+
 alphas = [1., 3., 6.]
-n_updates = 50
-lr = 0.2
+learning_rates = [0.04, 0.2, 1.]
+experiments = list(itertools.product(alphas, learning_rates))
+
+n_updates = 100
 
 
 # GDA
 
-def GDA_step(x, y, out_f, out_g):
+def GDA_step(x, y, out_f, out_g, eta=None):
     df_dx = torch.autograd.grad(outputs=out_f, inputs=x, create_graph=False)[0]
     dg_dy = torch.autograd.grad(outputs=out_g, inputs=y, create_graph=False)[0]
     return df_dx, dg_dy
@@ -29,19 +33,12 @@ def GDA_step(x, y, out_f, out_g):
 
 # CGD
 
-def CGD_step(x, y, out_f, out_g, eta=lr):
+def CGD_step(x, y, out_f, out_g, eta):
     df_dx, df_dy = torch.autograd.grad(outputs=out_f, inputs=[x, y], create_graph=True)
     dg_dx, dg_dy = torch.autograd.grad(outputs=out_g, inputs=[x, y], create_graph=True)
 
     d2f_dxdy = torch.autograd.grad(outputs=df_dx, inputs=y, create_graph=False)[0]
     d2g_dydx = torch.autograd.grad(outputs=dg_dy, inputs=x, create_graph=False)[0]
-
-    # # TODO: make sure we have the right order for cross-derivatives (or that the Hessian is symmetric)
-    #
-    # d2f_dydx = torch.autograd.grad(outputs=df_dy, inputs=x, create_graph=False)[0]
-    # d2g_dxdy = torch.autograd.grad(outputs=dg_dx, inputs=y, create_graph=False)[0]
-    #
-    # assert d2f_dxdy == d2f_dydx and d2g_dydx == d2g_dxdy
 
     step_x = ((1. / (1. - (eta ** 2.) * d2f_dxdy * d2g_dydx)) * (df_dx - eta * d2f_dxdy * dg_dy))
     step_y = ((1. / (1. - (eta ** 2.) * d2g_dydx * d2f_dxdy)) * (dg_dy - eta * d2g_dydx * df_dx))
@@ -51,19 +48,12 @@ def CGD_step(x, y, out_f, out_g, eta=lr):
 
 # LCGD (LOLA)
 
-def LCGD_step(x, y, out_f, out_g, eta=lr):
+def LCGD_step(x, y, out_f, out_g, eta):
     df_dx, df_dy = torch.autograd.grad(outputs=out_f, inputs=[x, y], create_graph=True)
     dg_dx, dg_dy = torch.autograd.grad(outputs=out_g, inputs=[x, y], create_graph=True)
 
     d2f_dxdy = torch.autograd.grad(outputs=df_dx, inputs=y, create_graph=False)[0]
     d2g_dydx = torch.autograd.grad(outputs=dg_dy, inputs=x, create_graph=False)[0]
-
-    # # TODO: make sure we have the right order for cross-derivatives (or that the Hessian is symmetric)
-    #
-    # d2f_dydx = torch.autograd.grad(outputs=df_dy, inputs=x, create_graph=False)[0]
-    # d2g_dxdy = torch.autograd.grad(outputs=dg_dx, inputs=y, create_graph=False)[0]
-    #
-    # assert d2f_dxdy == d2f_dydx and d2g_dydx == d2g_dxdy
 
     step_x = (df_dx - eta * d2f_dxdy * dg_dy)
     step_y = (dg_dy - eta * d2g_dydx * df_dx)
@@ -78,36 +68,43 @@ alg_updates = [GDA_step, LCGD_step, CGD_step]
 colors = ['cyan', 'orange', 'brown']
 markers = ['+', '<', 'o']
 
-recorder = {alg_name: {f'alpha={alpha}': [[x_init, y_init]] for alpha in alphas} for alg_name in alg_names}
+recorder = {alg_name: {f'alpha={alpha:.1f}, lr={lr:.2f}': [[x_init, y_init]] for alpha, lr in experiments} for alg_name in alg_names}
 for alg_name, alg_update in zip(alg_names, alg_updates):
+    for (alpha, lr) in experiments:
 
-    x = torch.tensor([x_init], requires_grad=True)
-    y = torch.tensor([y_init], requires_grad=True)
+        x = torch.tensor([x_init], requires_grad=True)
+        y = torch.tensor([y_init], requires_grad=True)
 
-    for alpha in alphas:
         for i in range(n_updates):
-            step_x, step_y = alg_update(x=x, y=y, out_f=f(x, y, alpha), out_g=g(x, y, alpha))
+            step_x, step_y = alg_update(x=x, y=y, out_f=f(x, y, alpha), out_g=g(x, y, alpha), eta=lr)
             x = x - lr * step_x
             y = y - lr * step_y
 
-            recorder[alg_name][f'alpha={alpha}'].append([x, y])
+            recorder[alg_name][f'alpha={alpha:.1f}, lr={lr:.2f}'].append([x, y])
 
 # Plotting
 
-fig, axes = plt.subplots(1, 3, figsize=(15, 4))
-for ax, alpha in zip(axes, alphas):
+fig, axes = plt.subplots(3, 3, figsize=(12, 10))
+for (j,i), (alpha, lr) in zip(itertools.product(range(3), range(3)), experiments):
 
     for alg_name, color, marker in zip(alg_names, colors, markers):
-        x_s, y_s = np.array(recorder[alg_name][f'alpha={alpha}']).T
-        ax.plot(x_s, y_s, marker=marker, color=color, fillstyle='none', linewidth=1, label=alg_name)
+        x_s, y_s = np.array(recorder[alg_name][f'alpha={alpha:.1f}, lr={lr:.2f}']).T
+        axes[i,j].plot(x_s, y_s, marker=marker, color=color, fillstyle='none', linewidth=1, label=alg_name)
 
-    ax.set_title(rf'$\alpha$={alpha}')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_xlim(-2, 2)
-    ax.set_ylim(-2, 2)
-    ax.locator_params('x', nbins=5)
-    ax.locator_params('y', nbins=5)
-    ax.legend(loc='upper right')
+    if i == 0:
+        axes[i, j].set_title(rf'$\alpha$={alpha:.1f}', fontsize=18)
+    if j == 2:
+        axes[i, j].text(s=rf'$\eta$={lr:.2f}', rotation=-35, x=2.2, y=-0.5, fontsize=18)
+    if i == 2:
+        axes[i, j].set_xlabel('x', fontsize=18)
+    if j == 0:
+        axes[i, j].set_ylabel('y', fontsize=18)
+    axes[i, j].set_xlim(-2, 2)
+    axes[i, j].set_ylim(-2, 2)
+    axes[i, j].tick_params(axis='both', which='major', labelsize=12)
+    axes[i, j].locator_params('x', nbins=3)
+    axes[i, j].locator_params('y', nbins=3)
+    axes[i, j].legend(loc='upper right')
 
 plt.show()
+fig.savefig(f'experiment1.png')
