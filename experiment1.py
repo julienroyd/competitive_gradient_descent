b02eb63e836399d5f2518844f960a9684da68263
JulienRoy <jul.roy1311@gmail.com>
2019-12-12 23:01:38 -0500

    Experiment1: Implemented LCGD (LOLA)

--- experiment1.py
+++ experiment1.py
@@ -49,11 +49,34 @@ def CGD_step(x, y, out_f, out_g, eta=lr):
     return step_x, step_y
 
 
+# LCGD (LOLA)
+
+def LCGD_step(x, y, out_f, out_g, eta=lr):
+    df_dx, df_dy = torch.autograd.grad(outputs=out_f, inputs=[x, y], create_graph=True)
+    dg_dx, dg_dy = torch.autograd.grad(outputs=out_g, inputs=[x, y], create_graph=True)
+
+    d2f_dxdy = torch.autograd.grad(outputs=df_dx, inputs=y, create_graph=False)[0]
+    d2g_dydx = torch.autograd.grad(outputs=dg_dy, inputs=x, create_graph=False)[0]
+
+    # # TODO: make sure we have the right order for cross-derivatives (or that the Hessian is symmetric)
+    #
+    # d2f_dydx = torch.autograd.grad(outputs=df_dy, inputs=x, create_graph=False)[0]
+    # d2g_dxdy = torch.autograd.grad(outputs=dg_dx, inputs=y, create_graph=False)[0]
+    #
+    # assert d2f_dxdy == d2f_dydx and d2g_dydx == d2g_dxdy
+
+    step_x = (df_dx - eta * d2f_dxdy * dg_dy)
+    step_y = (dg_dy - eta * d2g_dydx * df_dx)
+
+    return step_x, step_y
+
+
 # Execution loops
 
-alg_names = ['GDA', 'CGD']
-alg_updates = [GDA_step, CGD_step]
-colors = ['cyan', 'brown']
+alg_names = ['GDA', 'LCGD', 'CGD']
+alg_updates = [GDA_step, LCGD_step, CGD_step]
+colors = ['cyan', 'orange', 'brown']
+markers = ['+', '<', 'o']
 
 recorder = {alg_name: {f'alpha={alpha}': [[x_init, y_init]] for alpha in alphas} for alg_name in alg_names}
 for alg_name, alg_update in zip(alg_names, alg_updates):
@@ -74,9 +97,9 @@ for alg_name, alg_update in zip(alg_names, alg_updates):
 fig, axes = plt.subplots(1, 3, figsize=(15, 4))
 for ax, alpha in zip(axes, alphas):
 
-    for alg_name, color in zip(alg_names, colors):
+    for alg_name, color, marker in zip(alg_names, colors, markers):
         x_s, y_s = np.array(recorder[alg_name][f'alpha={alpha}']).T
-        ax.plot(x_s, y_s, marker='o', color=color, fillstyle='none', linewidth=1, label=alg_name)
+        ax.plot(x_s, y_s, marker=marker, color=color, fillstyle='none', linewidth=1, label=alg_name)
 
     ax.set_title(rf'$\alpha$={alpha}')
     ax.set_xlabel('x')
@@ -86,4 +109,5 @@ for ax, alpha in zip(axes, alphas):
     ax.locator_params('x', nbins=5)
     ax.locator_params('y', nbins=5)
     ax.legend(loc='upper right')
+
 plt.show()
