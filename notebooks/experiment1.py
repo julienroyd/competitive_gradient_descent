d3e0a39181a3a1550aca1a3c6efb04cd0f9ca602
JulienRoy <jul.roy1311@gmail.com>
2019-12-14 11:26:11 -0500

    Experiments1-2: Refactored to reuse some common code

--- notebooks/experiment1.py
+++ notebooks/experiment1.py
@@ -1,17 +1,17 @@
-import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
+from notebooks.experiments12_common import GDA_step, LCGD_step, CGD_step, run_experiment
 
 
-# The problem
+# Experiment 1
 
 def f(x, y, alpha):
     return alpha * x * y
 
 
-def g(x, y, alpha):
-    return - alpha * x * y
+def g(*f_args):
+    return - f(*f_args)
 
 x_init = 0.5
 y_init = 0.5
@@ -22,65 +22,12 @@ experiments = list(itertools.product(alphas, learning_rates))
 
 n_updates = 100
 
-
-# GDA
-
-def GDA_step(x, y, out_f, out_g, eta=None):
-    df_dx = torch.autograd.grad(outputs=out_f, inputs=x, create_graph=False)[0]
-    dg_dy = torch.autograd.grad(outputs=out_g, inputs=y, create_graph=False)[0]
-    return df_dx, dg_dy
-
-
-# CGD
-
-def CGD_step(x, y, out_f, out_g, eta):
-    df_dx = torch.autograd.grad(outputs=out_f, inputs=x, create_graph=True)[0]
-    dg_dy = torch.autograd.grad(outputs=out_g, inputs=y, create_graph=True)[0]
-
-    d2f_dxdy = torch.autograd.grad(outputs=df_dx, inputs=y, create_graph=False)[0]
-    d2g_dydx = torch.autograd.grad(outputs=dg_dy, inputs=x, create_graph=False)[0]
-
-    step_x = ((1. / (1. - (eta ** 2.) * d2f_dxdy * d2g_dydx)) * (df_dx - eta * d2f_dxdy * dg_dy))
-    step_y = ((1. / (1. - (eta ** 2.) * d2g_dydx * d2f_dxdy)) * (dg_dy - eta * d2g_dydx * df_dx))
-
-    return step_x, step_y
-
-
-# LCGD (LOLA)
-
-def LCGD_step(x, y, out_f, out_g, eta):
-    df_dx = torch.autograd.grad(outputs=out_f, inputs=x, create_graph=True)[0]
-    dg_dy = torch.autograd.grad(outputs=out_g, inputs=y, create_graph=True)[0]
-
-    d2f_dxdy = torch.autograd.grad(outputs=df_dx, inputs=y, create_graph=False)[0]
-    d2g_dydx = torch.autograd.grad(outputs=dg_dy, inputs=x, create_graph=False)[0]
-
-    step_x = (df_dx - eta * d2f_dxdy * dg_dy)
-    step_y = (dg_dy - eta * d2g_dydx * df_dx)
-
-    return step_x, step_y
-
-
-# Execution loops
-
 alg_names = ['GDA', 'LCGD', 'CGD']
 alg_updates = [GDA_step, LCGD_step, CGD_step]
 colors = ['cyan', 'orange', 'brown']
 markers = ['+', '<', 'o']
 
-recorder = {alg_name: {f'alpha={alpha:.1f}, lr={lr:.2f}': [[x_init, y_init]] for alpha, lr in experiments} for alg_name in alg_names}
-for alg_name, alg_update in zip(alg_names, alg_updates):
-    for (alpha, lr) in experiments:
-
-        x = torch.tensor([x_init], requires_grad=True)
-        y = torch.tensor([y_init], requires_grad=True)
-
-        for i in range(n_updates):
-            step_x, step_y = alg_update(x=x, y=y, out_f=f(x, y, alpha), out_g=g(x, y, alpha), eta=lr)
-            x = x - lr * step_x
-            y = y - lr * step_y
-
-            recorder[alg_name][f'alpha={alpha:.1f}, lr={lr:.2f}'].append([x, y])
+recorder = run_experiment(alg_names, alg_updates, experiments, x_init, y_init, f, g, n_updates)
 
 # Plotting
 
@@ -108,3 +55,4 @@ for (j,i), (alpha, lr) in zip(itertools.product(range(3), range(3)), experiments
 
 plt.show()
 fig.savefig(f'experiment1.png')
+plt.close(fig)
